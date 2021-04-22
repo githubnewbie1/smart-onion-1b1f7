70ef1816055f94d0bbea4b03f5476ee831213e44
githubnewbie1 <chrisdk.mail@gmail.com>
2021-04-22 18:21:55 +0000

    index.js: updated Typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const Home = (props) => {
           paragraph
           variant="h1"
         >
-          <span className={classes.angles}>&lt;</span> hello and welcome to my blockchain site{" "}
+          <span className={classes.angles}>&lt;</span> hello traveler{" "}
           <span className={classes.angles}>&gt;</span>
         </Typography>
         <Typography className={classes.h2} paragraph variant="h2">
