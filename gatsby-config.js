b08ee96c568eab3cc0eb568646007caefcf034b4
Stackbit Code Editor <projects@stackbit.com>
2021-04-22 18:35:51 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Customize me!
   siteMetadata: {
-    company: "IT Co",
+    company: "IT Con",
     domain: "chrisdk.crypto",
     defaultTitle:
       "Fox and Geese: Digital Agency Providing Serverless + React.js Engineering",
