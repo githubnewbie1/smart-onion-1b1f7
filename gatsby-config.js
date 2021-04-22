e91c4946ec57b377a64c85858cd86aea2d823f3a
Stackbit Code Editor <projects@stackbit.com>
2021-04-22 18:40:54 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     preamble:
       "I'm a Self Made Business Startup Consultant and Cost Effective Solutions Provider.",
     defaultDescription: "I connect you to Technology and Ensure a Profitatble Sustained Relationship",
-    postamble: "Think we can help your project? We'd love to hear from you:",
+    postamble: "Think I can help your project? We'd love to hear from you:",
     contact: {
       email: "hi@foxandgeese.com",
     },
