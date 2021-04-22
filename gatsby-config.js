b7c334e84a615924a8aa4699b2eaa612db51e9fc
Stackbit Code Editor <projects@stackbit.com>
2021-04-22 18:45:45 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Customize me!
   siteMetadata: {
-    company: "Consultancy, Startups, Sourcing and Investments",
+    company: "IT Consultancy, Startups, Sourcing and Investments",
     domain: "chrisdk.crypto",
     defaultTitle:
       "Consultancy, Startups, Sourcing and Investments: Providing IT Consultancy Services, Solutions, Cost Savings and SMB Startup Nerdtrepreneur",
